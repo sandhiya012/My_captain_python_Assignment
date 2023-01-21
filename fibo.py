665d072bbc725eb5606c8b8b9257236676c5ba8a
sandhiya012 <122517734+sandhiya012@users.noreply.github.com>
2023-01-21 12:33:37 +0530

    fibonacci series

---
+++ fibo.py
@@ -0,0 +1,13 @@
+n=int(input("Enter range of numbers needed= "))
+a=-1
+b=1
+c=0
+print("fibonacci series:")
+for i in range(0,n+1):
+      c=a+b
+      print(c)
+      a=b
+      b=c
+      
+      
+      
